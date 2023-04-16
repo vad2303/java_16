e6b95f443ee568e67231a8437f1cd1cb3d9349a1
vad2303 <126171119+vad2303@users.noreply.github.com>
2023-04-16 15:39:11 +0700

    commit note

---
+++ BaiThucHanhLab2/baithuchanh1003/src/slide74.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class slide74{
+    public static void main(String[] args) {
+        System.out.println("Nhap so phan tu trong mang: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double[] arr = new double[n];
+        double total = 0;
+        for(int i=0; i<arr.length; i++){
+            System.out.print("Nhap gia tri cho phan tu trong mang "+(i+1)+": ");
+            arr[i] = sc.nextDouble();
+        }
+        sc.close();
+        for(int i=0; i<arr.length; i++){
+            total = total + arr[i];
+        }
+        System.out.format("Kết quả là: %.3f", total);
+    }
+}
+    
---
+++ BaiThucHanhLab2/baithuchanh2402/src/slide35.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class slide35 {
+    public static void main(String[] args) {
+    try (Scanner sc = new Scanner(System.in)) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a > b);
+        }
+  }
+
+}
