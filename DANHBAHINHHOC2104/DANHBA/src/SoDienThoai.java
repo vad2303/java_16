ceb3eeae1fab9a17d1546c4143fcfbea5c060f5e
vad2303 <126171119+vad2303@users.noreply.github.com>
2023-04-21 12:30:47 +0700

    Delete DANHBAHINHHOC2104 directory

--- DANHBAHINHHOC2104/DANHBA/src/SoDienThoai.java
+++
@@ -1,9 +0,0 @@
-public abstract class SoDienThoai {
-    abstract void nhapSoDienThoai(String ten, String sodienthoai);
-
-    abstract void xoaSoDienThoai(String ten);
-
-    abstract void capnhatSoDienThoai(String ten, String sodienthoaimoi);
-
-    abstract void timSoDienThoai(String ten);
-}
